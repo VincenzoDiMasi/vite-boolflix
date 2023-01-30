0162c7bb0dc76f36f14966c004ca26f2ce786529
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-01-30 11:45:54 +0100

    Add hover & style

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -11,6 +11,11 @@
             name: String,
             original_name: String,
             product: Object,
+        },
+        data() {
+            return {
+                hover: false
+            }
         }
     }
 </script>
@@ -18,33 +23,47 @@
 <template>
     <div class="col mb-3">
        
-        <figure>
-            <img class="poster" :src="`https://image.tmdb.org/t/p/w342/${product.poster_path}`" :alt="title">
-        </figure>   
-        
-        <div>
-            {{ product.title || product.name }}        
-        </div>
-        <div>
-        {{ product.original_title || product.original_name }}        
-        </div>
-        <img v-if="product.original_language === 'it' || product.original_language === 'en'" class="img-fluid flag" :src="`/src/assets/img/${product.original_language}.png`" :alt="product.original_language">
-        <div v-else>{{ product.original_language }}</div> 
+        <figure  @mouseover="hover = true" @mouseleave="hover = false">
+            <img class="poster" :src="`https://image.tmdb.org/t/p/w342/${product.poster_path}`" :alt="product.title">
 
-        <div>    
-            <div class="empty-star">
-            <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
+            <div v-if="hover" class="content-hvr h-100 w-100 d-flex flex-column  p-2">
+                <div>
+                    <h4>
+                        {{ product.title || product.name }}        
+                    </h4>
+                    <h6>
+                      Original title: {{ product.original_title || product.original_name }}     
+                    </h6>
+                </div>
+                <div >
+                    <img v-if="product.original_language === 'it' || product.original_language === 'en'" class="img-fluid flag" :src="`/src/assets/img/${product.original_language}.png`" :alt="product.original_language">
+                    <div v-else>{{ product.original_language }}</div>
+                </div>
+                
+                <div>
+                    
+                    <div class="empty-star">
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
 
-                <div class="full-star">
-                    <i v-for="n in Math.ceil(product.vote_average / 2)" class="fa-solid fa-star"></i>
+                        <div class="full-star">
+                            <i v-for="n in Math.ceil(product.vote_average / 2)" class="fa-solid fa-star"></i>
+                        </div>     
+                    </div>
+
+                    <div>
+                        <h5> Overview:</h5>
+                        <p>
+                            {{ product.overview }}
+                        </p>           
+                    </div>
+
                 </div>
             </div>
-
-        </div>
+        </figure>   
     </div>
    
 
@@ -52,12 +71,35 @@
 
 <style lang="scss" scoped>
    .flag {
-  height: 18px;
+  height: 25px;
 }
 
-.poster {
+figure {
+    position: relative;
+
+    .poster {
     height: 350px;
     cursor: pointer;
+    }
+
+    .content-hvr {
+                cursor: pointer;
+                position: absolute;
+                top: 0;
+                left: 0;
+                color: white;
+                background-color: rgba(0, 0, 0, 0.8);
+                overflow-y: auto;
+                overflow-x: hidden;
+
+                p {
+                    font-size: 12px;
+                }
+
+                *{
+                padding: 5px 0;
+            }
+    }
 }
 
 .empty-star {
